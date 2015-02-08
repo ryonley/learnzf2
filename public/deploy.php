23dc0c76e8739f726d2e6e2f5b3b993a9267aef0
Rodger <festool@festools-MacBook-Pro.local>
2015-02-08 13:49:34 -0600

    added empty deploy script

---
+++ public/deploy.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: festool
+ * Date: 2/8/15
+ * Time: 1:47 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+echo "this is a test";
